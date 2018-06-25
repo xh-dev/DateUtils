d4cb842f673efc2d2fb4692d88a2a63518d6f786
DESKTOP-MM3VFOH\Xeth-Surface-Pro <adamhung12@gmail.com>
2018-06-25 14:11:05 +0800

    add calendar get weekday

--- src/main/java/me/xethh/utils/dateManipulation/Weekday.java
+++ src/main/java/me/xethh/utils/dateManipulation/Weekday.java
@@ -1,5 +1,8 @@
 package me.xethh.utils.dateManipulation;
 
 public enum Weekday {
-    Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday
+    Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday;
+    public int getCalWeekDay(){
+        return ordinal()+1;
+    }
 }
