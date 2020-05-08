e0aa17bd57032b89db5fed2a2ca726ff97a82b9b
Xeth Hung <xcwhung@ogcio.gov.hk>
2020-05-08 18:05:34 +0800

    update

--- src/main/java/me/xethh/utils/dateUtils/weekday/Weekday.java
+++ src/main/java/me/xethh/utils/dateUtils/weekday/Weekday.java
@@ -1,14 +1,41 @@
 package me.xethh.utils.dateUtils.weekday;
 
+/**
+ * Enum for weekday
+ */
 public enum Weekday {
     Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday;
 
+    /**
+     * Weekday in number form <br>
+     * 1 {@literal ->} Monday <br>
+     * 2 {@literal ->} Tuesday <br>
+     * 3 {@literal ->} Wednesday <br>
+     * 4 {@literal ->} Thursday <br>
+     * 5 {@literal ->} Friday <br>
+     * 6 {@literal ->} Saturday <br>
+     * 7 {@literal ->} Sunday <br>
+     *
+     * @return number liberal of weekday
+     */
     public int getCalWeekDay() {
         if (ordinal() == 0)
             return 7;
         return ordinal();
     }
 
+    /**
+     * Weekday from ordinal form <br>
+     * 0 {@literal ->} Sunday <br>
+     * 1 {@literal ->} Monday <br>
+     * 2 {@literal ->} Tuesday <br>
+     * 3 {@literal ->} Wednesday <br>
+     * 4 {@literal ->} Thursday <br>
+     * 5 {@literal ->} Friday <br>
+     * 6 {@literal ->} Saturday <br>
+     *
+     * @return Weekday
+     */
     public static Weekday getByOrdinal(int day) {
         if (day < 0 || day > 6)
             throw new RuntimeException(
@@ -19,6 +46,19 @@ public enum Weekday {
         throw new RuntimeException(String.format("Suitable value not found [value input is %d] ", day));
     }
 
+    /**
+     * Weekday from number literal form <br>
+     * 0 {@literal ->} Sunday <br>
+     * 1 {@literal ->} Monday <br>
+     * 2 {@literal ->} Tuesday <br>
+     * 3 {@literal ->} Wednesday <br>
+     * 4 {@literal ->} Thursday <br>
+     * 5 {@literal ->} Friday <br>
+     * 6 {@literal ->} Saturday <br>
+     * 7 {@literal ->} Sunday <br>
+     *
+     * @return Weekday
+     */
     public static Weekday getByCalWeekday(int day) {
         if (day < 1 || day > 7)
             throw new RuntimeException(String.format("Day should not larger than 7 or smaller than 1 [value input is %d] ", day));
@@ -28,6 +68,11 @@ public enum Weekday {
             return getByOrdinal(day);
     }
 
+    /**
+     * The next weekday <br>
+     * e.g. Saturday {@literal ->} Sunday <br>
+     * @return Weekday
+     */
     public Weekday nextDay() {
         if (ordinal() == 6)
             return Sunday;
@@ -35,6 +80,11 @@ public enum Weekday {
             return getByOrdinal(ordinal() + 1);
     }
 
+    /**
+     * The previous weekday <br>
+     * e.g. Monday {@literal ->} Sunday
+     * @return Weekday
+     */
     public Weekday prevDay() {
         if (ordinal() == 0)
             return Saturday;
